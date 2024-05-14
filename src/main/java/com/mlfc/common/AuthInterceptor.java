19adf06864569cc9d4922377100309650b716f0c
mlfc <mlfc0422@outlook.com>
2024-05-14 21:18:19 +0800

    加入了拦截器

---
+++ src/main/java/com/mlfc/common/AuthInterceptor.java
@@ -0,0 +1,35 @@
+package com.mlfc.common;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("拦截请求：{}", request.getRequestURI());
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user") == null) {
+            log.info("用户未登录，拦截请求：{}", request.getRequestURI());
+            response.setStatus(401);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
--- src/main/java/com/mlfc/config/MvcConfig.java
+++ src/main/java/com/mlfc/config/MvcConfig.java
@@ -3,17 +3,24 @@ package com.mlfc.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mlfc.common.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan("com.mlfc.controller")
 public class MvcConfig implements WebMvcConfigurer {
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         // 创建一个 ObjectMapper 对象，用于 JSON 数据的序列化和反序列化
@@ -32,5 +39,4 @@ public class MvcConfig implements WebMvcConfigurer {
         // 将这个转换器添加到传入的 converters 列表中，以便在 Spring MVC 中使用
         converters.add(converter);
     }
-
 }
